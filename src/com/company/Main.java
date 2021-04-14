21a0071f4c6d033875cfb4348d4fe60b5f3cceb5
Mary Kazaryan <82213157+mary-kazaryan@users.noreply.github.com>
2021-04-15 00:56:25 +0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //  Необходимо, чтоб программа выводила на экран вот такую последовательность:
+        //  7 14 21 28 35 42 49 56 63 70 77 84 91 98.
+        //  В решении используйте цикл while.
+
+        int number = 7;
+
+        while (number < 99) {
+            System.out.println(number);
+            number = number + 7;
+            }
+        }
+
+
+
+
+    }
